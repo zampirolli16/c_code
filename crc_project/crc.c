aa4007e618d8b6b97700b5199bb31cce5b6e2f75
zampiroli16 <zampirolli@usp.br>
2025-10-26 00:50:34 -0300

    Verificação do crc aparentemente funcional

--- crc_project/crc.c
+++ crc_project/crc.c
@@ -106,10 +106,15 @@ void main (void){
     uint16_t result_crc;
     uint16_t verif;
     uint8_t verif_array[7] = {0x29, 0xC0, 0x0B, 0x0F, 0x02, 0x00, 0x80};
-
+    
+    
+    printf ("Calcucando CRC \n");
     result_crc = calculate_crc16(&data[0], bytes, 0);
-    verif = calculate_crc16(&data[0], bytes + 2, 1);
-    printf("fim\n");
+   
+    printf ("-------------------------------\n");
+    
+    printf ("Verificando o CRC calculado \n");
+    verif = calculate_crc16(&verif_array[0], bytes + 2, 1);
 }
 
 void shift_left_array(uint8_t* vetor, size_t tamanho);
